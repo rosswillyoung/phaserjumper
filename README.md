4a807589aef754763549ad8e1a90a39fe9a26b73
Ross Young <rosswillyoung@gmail.com>
2022-01-11 12:05:34 -0500

    Added some platform generation, and main camera scrolling functionality while keeping the background

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 Simple Jumper Game I'm creating to test out Phaser and brush up on JavaScript/Github
 
 - [x] Add ground and sky
-- [ ] Add platforms
+- [x] Add platforms
 - [x] Add Sprite
 - [x] Jumping and moving mechanism for sprite
 - [x] Animation frames for sprite
-- [ ] Auto-generate and remove platforms
+- [x] Auto-generate platforms
+- [ ] Remove off-screen platforms
+- [x] Collision detection just for top of platform
+- [ ] Scroll speed temporarily higher when player is above a certain height
+    - [ ] Need to keep in mind platform creation
+- [ ] Update platform creation to be better (more varied height and widths)
 - [ ] Set scroll speed higher as time goes on
 - [ ] Power-ups
 - [ ] Shooting mechanism
--- main.js
+++ main.js
@@ -23,22 +23,31 @@ let stars
 let bombs
 let platforms
 let cursors
+let playerHeight
+let currentHeight = -600
+let lastPlatformHeight = 600
 
 function preload() {
     this.load.image('sky', 'assets/sky.png')
     this.load.image('ground', 'assets/platform.png')
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 })
-
 }
+
 function create() {
-    this.add.image(400, 300, 'sky')
+    // this.physics.world.setBounds(0, 0, 800, 900000)
+    let background = this.add.image(400, 300, 'sky')
+    // make background static
+    background.setScrollFactor(0)
+
     platforms = this.physics.add.staticGroup()
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+    let ground = platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+
+
 
     player = this.physics.add.sprite(100, 450, 'dude')
     player.setBounce(0.2)
-    player.setCollideWorldBounds(true)
+    // player.setCollideWorldBounds(true)
 
     this.anims.create({
         key: 'left',
@@ -62,15 +71,24 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys()
 
-    this.physics.add.collider(player, platforms)
+    this.physics.add.collider(player, ground)
 }
 function update() {
+    let cam = this.cameras.main
+
+    // console.log(player.y)
     if (cursors.left.isDown) {
         player.setVelocityX(-160)
         player.anims.play('left', true)
+        if (player.x < 0) {
+            player.x += 800
+        }
     } else if (cursors.right.isDown) {
         player.setVelocityX(160)
         player.anims.play('right', true)
+        if (player.x > 800) {
+            player.x -= 800
+        }
     } else {
         player.setVelocityX(0)
         player.anims.play('turn')
@@ -79,4 +97,30 @@ function update() {
     if (player.body.touching.down) {
         player.setVelocityY(-330)
     }
+
+    // Generate Platforms
+
+    if (currentHeight < lastPlatformHeight) {
+        platform = generatePlatform()
+        this.physics.add.collider(player, platform)
+    }
+
+    currentHeight -= .2
+    console.log(currentHeight, lastPlatformHeight)
+    // move camera up
+    cam.scrollY -= .2
+
+}
+
+function generatePlatform() {
+    x = Phaser.Math.Between(0, 800)
+    y = Phaser.Math.Between(-10, 10)
+    platform = platforms.create(x, lastPlatformHeight, 'ground')
+    lastPlatformHeight -= 150 + y
+    console.log('platform created', platform.x, platform.y)
+    platform.body.checkCollision.down = false
+    platform.body.checkCollision.right = false
+    platform.body.checkCollision.left = false
+    return platform
+
 }
